589704dc2ce5f52b8d9c05fec77723ddcf76c404
Caen De Silva <caen@desilva.se>
2024-03-23 11:55:40 +0100

    Update index.blade.php

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -1,11 +1,11 @@
-@php($title = 'Latest Posts')
+@php($title = 'Latest Tips')
 @extends('hyde::layouts.app')
 @section('content')
 
     <main id="content" class="mx-auto max-w-7xl py-12 px-8">
         <header class="lg:mb-12 xl:mb-16">
             <h1 class="text-3xl text-left leading-10 tracking-tight font-extrabold sm:leading-none mb-8 md:mb-12 md:text-4xl md:text-center lg:text-5xl text-gray-700 dark:text-gray-200">
-                Latest Posts
+                Latest Tips
             </h1>
         </header>
 
