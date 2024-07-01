ae5e8357cf6142123af44b0d21ceba038527cb8e
Caen De Silva <caen@desilva.se>
2024-07-01 11:36:53 +0200

    Update page title

--- _pages/index.blade.php
+++ _pages/index.blade.php
@@ -1,11 +1,11 @@
-@php($title = 'Latest Tips')
+@php($title = 'Latest Coding Tips')
 @extends('hyde::layouts.app')
 @section('content')
 
     <main id="content" class="mx-auto max-w-7xl py-12 px-8">
         <header class="lg:mb-12 xl:mb-16">
             <h1 class="text-3xl text-left leading-10 tracking-tight font-extrabold sm:leading-none mb-8 md:mb-12 md:text-4xl md:text-center lg:text-5xl text-gray-700 dark:text-gray-200">
-                Latest Tips
+                Latest Coding Tips
             </h1>
         </header>
 
